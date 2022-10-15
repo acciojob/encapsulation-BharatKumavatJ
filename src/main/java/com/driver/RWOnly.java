835fcb6341b1660d49d426bf30676fb3d85b1e13
bharat <bharatkumarchuahan179@gmail.com>
2022-10-15 20:44:54 +0530

    encaptualation done

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public void setter(String name){
+        this.name = name;
+    }
+
+    public String getter(){
+        return this.name;
+    }
 }
+
+//
+// Task 5:
+//         try implementing getter and setters member function in class RWOnly
+// Task 6:
+//        Now, try setting a value to name in Main function using setter function and access it's value using getter function
+//        Learnings:
+//        Encapsulation is used for hidding data,
+//        you can create a readOnly member, by just implementing getter funtion for that
+//        Or you can also create a writeOnly member, by just implementing setter function for that */
